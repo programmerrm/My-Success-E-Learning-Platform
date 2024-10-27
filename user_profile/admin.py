5b48eb171518a06c17a9e7d794083b40eb4aa491
Rasel Mahmud <programmerwebrm@gmail.com>
2024-10-28 01:01:57 +0600

    new change

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import WithdrawalMethod, WithdrawalProcess
 
 # Register your models here.
+
+class WithdrawalMethodAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+class WithdrawalProcessAdmin(admin.ModelAdmin):
+    list_display = ['method', 'number', 'amount']
+
+admin.site.register(WithdrawalMethod, WithdrawalMethodAdmin)
+admin.site.register(WithdrawalProcess, WithdrawalProcessAdmin)
--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class WithdrawalMethod(models.Model):
+    name = models.CharField(
+        max_length=80,
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name or "Unnamed Method"
+
+class WithdrawalProcess(models.Model):
+    method = models.ForeignKey(
+        WithdrawalMethod,
+        on_delete=models.CASCADE,
+    )
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+    number = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return f"Withdrawal of {self.amount} using {self.method}"
