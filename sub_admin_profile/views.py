33a1cc32af873c759f4e3f138b1c1094e00b875e
Rasel Mahmud <programmerwebrm@gmail.com>
2024-11-01 22:51:15 +0600

    new chnage

--- sub_admin_profile/views.py
+++ sub_admin_profile/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from global_futures.models import LogoImage, FooterCopyRightText
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib import messages
 
 class SubAdminBaseTemplateView(TemplateView):
     template_name = 'sub_admin_profile/index.html'
@@ -17,12 +19,22 @@ class SubAdminBaseTemplateView(TemplateView):
         return context
 
 class AllStudent(SubAdminBaseTemplateView):
-    # This class will inherit from SubAdminBaseTemplateView
-    # The specific template can be handled in the index.html
     pass
 
 class UpdatedProfile(SubAdminBaseTemplateView):
     pass
 
 class ChangePassword(SubAdminBaseTemplateView):
-    pass
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = PasswordChangeForm(user=self.request.user)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Password change successfully")
+        context = self.get_context_data()
+        context['form'] = form
+        return render(request, self.template_name, context)
